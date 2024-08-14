36e81c1308e894788d36ce731c7bf9bdc57efc05
Rizbi Islam <rizbi.cse@gmail.com>
2024-08-14 18:32:37 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Greeting App',
+      home: GreetingScreen(),
+    );
+  }
+}
+
+class GreetingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Greeting App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello, World!',
+              style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            Text(
+              'Welcome to Flutter!',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 20),
+            Image.network(
+              'https://flutter.dev/images/flutter-logo-sharing.png',
+              height: 100,
+            ),
+            SizedBox(height: 40),
+            Image.asset('assets/flutter_icon.jpg'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                final snackBar = SnackBar(content: Text('Button Pressed!'));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              child: Text('Press me'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
